772df42aff4b780030b5b85e893936b8c140152a
pogsonlab <pogsonlab@rsb-33-253.anu.edu.au>
2013-05-02 17:51:13 +1000

    added stuff from pete's notebook, up to viewing fastqc result

--- 01-qc.sh
+++ 01-qc.sh
@@ -1,3 +1,20 @@
 #!/bin/bash
+# Get the number of cores to use. = num processeors - 1-min load average - 1
+nthreads=$(($(nproc) - $(uptime |grep -oP ": \d+\." |sed -e "s/^\: //" |sed -e "s/\.$//") - 1))
 
+# enter directory containing reads
+cd /media/work/pete/Project_SN700819R_0100_PCrisp_RSB_Arabidopsis_mRNA/
+
+### DO QC
+mkdir QC
+
+# run fastqc
+fastqc Sample_*/*.fastq.gz -o QC -t $nthreads
+
+
+# and view the results (remember Ctrl-tab changes tabs)
+for qc_res in QC/BJP_*_fastqc/fastqc_report.html
+do
+	firefox "file://$(readlink -f $qc_res)" &
+done
 
