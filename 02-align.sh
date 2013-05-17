ef0491dfb8f5a84ff11011469923b8def25ab8a8
Pete Crisp <peter.crisp@anu.edu.au>
2013-05-17 10:16:14 +1000

    happy with the aligment script for now

--- 02-align.sh
+++ 02-align.sh
@@ -2,7 +2,9 @@
 
 Project="Project_SN700819R_0100_PCrisp_RSB_Arabidopsis_mRNA"
 WSDir="/home/pete/workspace/"
-RefDir="~/workspace/refseqs"
+RefDir="/home/pete/workspace/refseqs"
+
+ProjectDir="${WSDir}/${Project}"
 
 # Make mapping dir
 cd "${WSDir}/${Project}"
@@ -10,21 +12,22 @@ mkdir "mapping"
 cd mapping
 
 # Make the Sample_* dirs in the mapping folder
-#for sample in Sample_*
-#do
-#	mkdir "${WSDir}/${Project}/mapping/${sample}"
-#done
-#find -maxdepth 1 -name Sample_\* -type d |parallel 
+for sample in ${ProjectDir}/Sample_*
+do
+	sample="$(basename "$sample")"
+	WorkingDir="${WSDir}/${Project}/mapping/${sample}"
+	mkdir ${WorkingDir} 
+	cd "${WorkingDir}"
 
-sample=Sample_BJP_317_1_1
-SampleDir="${WSDir}/${Project}/${sample}"
+	SampleDir="${WSDir}/${Project}/${sample}"
 
-WorkingDir="${WSDir}/${Project}/mapping/${sample}"
-
-mkdir "${WorkingDir}"
-
-cd "${WorkingDir}"
-
-pwd
-
-echo tophat2 -G "${RefDir}/TAIR10_gen/TAIR10_GFF3_genes_transposons.gff" --transcriptome-index "${RefDir}/TAIR10_tx_from_tophat/TAIR10_tx" --solexa-quals --library-type fr-unstranded -p 22 -g 2 ${RefDir}/TAIR10_gen/TAIR10 ${SampleDir}/*.fq.gz
+	tophat2 \
+		-G "${RefDir}/TAIR10_gen/TAIR10_GFF3_genes_transposons.gff" \
+		--transcriptome-index "${RefDir}/TAIR10_tx_from_tophat/TAIR10_tx" \
+		--solexa-quals \
+		--library-type fr-unstranded \
+		-p 22 \
+		-g 2 \
+		${RefDir}/TAIR10_gen/TAIR10 \
+		${SampleDir}/*.fq.gz | tee "./tophat${sample}.log"
+done
