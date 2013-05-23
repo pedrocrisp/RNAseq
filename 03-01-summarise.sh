63ad71948edb320864faf9fbc9d0a5217067d573
Pete Crisp <peter.crisp@anu.edu.au>
2013-05-23 16:53:39 +1000

    added beginnings of the htseq-edgeR pipeline

---
+++ 03-01-summarise.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+RefDir="/home/pete/workspace/refseqs"
+
+NTHREADS=1 # Default number of threads, in case we forget to updateNThreads
+function updateNThreads {
+	# Get the number of cores to use. = num processeors - 1-min load average - 1.
+	NTHREADS=$(($(nproc) - $(uptime |grep -oP ": \d+\." |sed -e "s/^\: //" |sed -e "s/\.$//") - 1))
+	echo "Can use $NTHREADS threads"
+}
+
+# enter directory containing reads
+cd /home/pete/workspace/Project_SN700819R_0100_PCrisp_RSB_Arabidopsis_mRNA/mapping
+
+#number of threads to use
+updateNThreads
+
+# Summarise read counts per gene with htseq; in parallel
+# not strand specific
+# 
+# mode is to keep partially multimapping reads if they have a portion that unambigiously aligns (multimapped called ambugious)
+	
+find -maxdepth 2  -type d  -name Sample_\* | parallel -j $NTHREADS "cd {}; htseq-count \
+	--stranded no \
+	-i ID \
+	-t gene \
+	-m intersection-nonempty \
+	tophat_out/accepted_hits_sorted.sam \
+	${RefDir}/TAIR10_gen/TAIR10_GFF3_genes_transposons.gff \
+	> tophat_out/accepted_hits.gene.counts \
+	>./htseq-count.log 2>&1"
+
+
