7ed41725cc6f83e918f046558ab817d22fb3929e
pedrocrisp <peter.crisp@anu.edu.au>
2013-07-28 17:47:13 +1000

    added igv_from_tophat script, sorts/indexes tophat bams

---
+++ igv_from_tophat.sh
@@ -0,0 +1,16 @@
+#/bin/bash
+
+cd "/home/pete/workspace/Project_SN700819R_0100_PCrisp_RSB_Arabidopsis_mRNA"
+igvDirName="igv_bams"
+
+
+mkdir $igvDirName
+
+#find mapping/ -type d -name Sample_BJP_317_1_3 | \
+find mapping/ -type d -name Sample\* | \
+	parallel \
+		mkdir -p $igvDirName/{/}/ \; \
+		samtools sort -m 3G {}/tophat_out/accepted_hits.bam $igvDirName/{/}/{/} \; \
+		samtools index $igvDirName/{/}/{/}.sorted.bam
+
+
