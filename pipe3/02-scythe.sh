74de456b61e3b0318777591592fe442fb26f2ad1
Peter Crisp <pedrocrisp@gmail.com>
2014-02-14 09:26:45 +1100

    started scythe scripts

---
+++ pipe3/02-scythe.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+set -e
+set -x
+ 
+if [ $# -ne 1 ]
+then
+echo "USAGE: fastqc.sh SAMPLENAME"
+fi
+
+#user to define
+refdir=
+
+sample=$1
+sample_dir=reads/$sample
+ 
+fastqs="$(ls $sample_dir/*.fastq.gz)"
+ 
+mkdir qc/$sample
+ 
+scythe \
+-p 0.1 \ # set prior to 0.1 (default 0.3)
+-a {refdir}/truseq_adapters.fasta \ #adapter file
+-o qcd/$sample \ #
+$fastqs #input
