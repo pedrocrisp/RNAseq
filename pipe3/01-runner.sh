acaf2fd9b2d1839358988b233a671f4b70ca78b8
Peter Crisp <pedrocrisp@gmail.com>
2014-02-13 19:09:07 +1100

    added mkdir qc to runner to fix parllel bug, hard code paths

--- pipe3/01-runner.sh
+++ pipe3/01-runner.sh
@@ -2,5 +2,17 @@ function findSamples () {
 find reads/ -mindepth 1 -maxdepth 1 -type d  -exec basename {} \;| tr ' ' '\n'
 }
 
-findSamples | parallel bash fastqc.sh {}
+mkdir qc
 
+#make sure path is correct
+#pete@macpro
+findSamples | parallel bash ~/gitrepos/RNAseq/pipe3/01-fastqc.sh {}
+
+
+#pete@north1ws:
+#findSamples | parallel bash ~ws/fastqc.sh {}
+
+#to run got to the folder containing the reads folder:
+#cd ~/ps/xgames/exp277_mutants/
+#Then:
+#bash ~/gitrepos/RNAseq/pipe3/01-runner.sh
