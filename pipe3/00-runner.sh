6449291d3a18647c6c95af70d66406844af05c9e
Peter Crisp <pedrocrisp@gmail.com>
2014-02-19 20:26:08 +1100

    rearranged cat.sh and runner

--- pipe3/00-runner.sh
+++ pipe3/00-runner.sh
@@ -1,17 +1,16 @@
+###
+#user defined variables:
+workingdir=~/alx_test
+scriptfile=~/bec_pipeline/pipe3/00-cat.sh
+###
+
 function findSamples () {
 find reads/ -mindepth 1 -maxdepth 1 -type d  -exec basename {} \;| tr ' ' '\n'
 }
 
-###
-#user defined variables:
-workingdir=~/alx_test
-scriptdir=~/bec_pipeline/pipe3
-###
-
 cd $workingdir
 
-#make sure path is correct
-#pete@macpro
-findSamples | parallel bash "$scriptdir/00-cat.sh"
+findSamples | parallel bash $scriptfile
 
+#To run:
 #bash ~/bec_pipeline/pipe3/00-runner.sh
