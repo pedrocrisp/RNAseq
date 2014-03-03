6a1ba27c41d28b22c5d5da933f8d6b0cc76b8eef
Peter Crisp <pedrocrisp@gmail.com>
2014-03-03 15:13:31 +1100

    01 softcoded and tested

--- pipe3/01-runner.sh
+++ pipe3/01-runner.sh
@@ -1,8 +1,7 @@
 ###
-#user defined variables:
+#user defined variables that could be changed:
 workingdir=./
 script=~/gitrepos/RNAseq/pipe3/01-fastqc.sh
-runner=~/gitrepos/RNAseq/pipe3/01-runner.sh
 outdir=reads_fastqc
 ###
 
@@ -10,18 +9,18 @@ function findSamples () {
 find reads/ -mindepth 1 -maxdepth 1 -type d  -exec basename {} \;| tr ' ' '\n'
 }
 
-cd $workingdir
 mkdir ${outdir}
 timestamp=$(date +%Y%m%d-%H%M%S)
 
+mkdir logs
 logdir="./logs/${outdir}.${timestamp}"
 mkdir $logdir
 
 cat $script > "$logdir/script.log"
-cat $runner > "$logdir/runner.log"
+cat $0 > "$logdir/runner.log"
 cat $script
 
 findSamples | parallel bash $script {} \>logs/${outdir}.${timestamp}/{}.log 2\>\&1
 
-#To run:
+#To run, got to directory containing reads directory and call:
 #bash ~/path_to/01-runner.sh
