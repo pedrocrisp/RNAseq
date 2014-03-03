01a81e53ae054dfc420d61943d715f5ff3a8a5ef
Peter Crisp <pedrocrisp@gmail.com>
2014-03-03 18:50:33 +1100

    02 softcoded

--- pipe3/02-runner.sh
+++ pipe3/02-runner.sh
@@ -1,7 +1,18 @@
 ###
-#user defined variables:
+#code to make it work on osx and linux
+if
+[[ $OSTYPE == darwin* ]]
+then
+readlink=$(which greadlink)
+scriptdir="$(dirname $($readlink -f $0))"
+else
+scriptdir="$(dirname $(readlink -f $0))"
+fi
+#
+
+#user defined variables that could be changed:
 workingdir=./
-script=~/gitrepos/RNAseq/pipe3/02-scythe.sh
+script=$scriptdir/02-scythe.sh
 outdir=reads_scythe
 ###
 
@@ -9,7 +20,6 @@ function findSamples () {
 find reads/ -mindepth 1 -maxdepth 1 -type d  -exec basename {} \;| tr ' ' '\n'
 }
 
-cd $workingdir
 mkdir $outdir
 timestamp=$(date +%Y%m%d-%H%M%S)
 
@@ -17,10 +27,11 @@ logdir="./logs/${outdir}.${timestamp}"
 mkdir $logdir
 
 cat $script > "$logdir/script.log"
-cat $runner > "$logdir/runner.log"
+cat $0 > "$logdir/runner.log"
 cat $script
 
 findSamples | parallel bash $script {} \>logs/${outdir}.${timestamp}/{}.log 2\>\&1
 
-#To run:
+#To run, got to directory containing reads directory and call:
 #bash ~/path_to/02-runner.sh
+
