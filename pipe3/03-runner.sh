1b9d731bbc487db6bc066836c702c1fc6bd570ec
Peter Crisp <pedrocrisp@gmail.com>
2014-03-04 15:09:54 +1100

    softcoded 03

--- pipe3/03-runner.sh
+++ pipe3/03-runner.sh
@@ -1,8 +1,18 @@
 ###
-#user defined variables:
+#code to make it work on osx and linux
+if
+[[ $OSTYPE == darwin* ]]
+then
+readlink=$(which greadlink)
+scriptdir="$(dirname $($readlink -f $0))"
+else
+scriptdir="$(dirname $(readlink -f $0))"
+fi
+#
+
+#user defined variables that could be changed:
 workingdir=./
-script=~/gitrepos/RNAseq/pipe3/03-seqtk.sh
-runner=~/gitrepos/RNAseq/pipe3/03-runner.sh
+script=$scriptdir/03-seqtk.sh
 outdir=reads_scythe_seqtk
 ###
 
@@ -10,7 +20,6 @@ function findSamples () {
 find reads_scythe/ -mindepth 1 -maxdepth 1 -type d  -exec basename {} \;| tr ' ' '\n'
 }
 
-cd $workingdir
 mkdir $outdir
 timestamp=$(date +%Y%m%d-%H%M%S)
 
@@ -18,7 +27,7 @@ logdir="./logs/${outdir}.${timestamp}"
 mkdir $logdir
 
 cat $script > "$logdir/script.log"
-cat $runner > "$logdir/runner.log"
+cat $0 > "$logdir/runner.log"
 cat $script
 
 findSamples | parallel bash $script {} \>logs/${outdir}.${timestamp}/{}.log 2\>\&1
