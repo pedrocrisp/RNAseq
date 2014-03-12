24cbacfee7d1e9cb26a8961b67513d396add31b7
Peter Crisp <pedrocrisp@gmail.com>
2014-03-12 23:04:23 +1100

    fixed .chrom.sizes in 07 added to .gitignore

--- pipe3/07-bam_to_tdf.sh
+++ pipe3/07-bam_to_tdf.sh
@@ -16,9 +16,10 @@ fi
 #
 
 ###
-#reference sequence directory variable - user should create a link called TAIR10_gen_chrc.genome.sizes in script dir that points to the TAIR10_gen_chrc.genome.sizes file, or put a copy in there (chrc means we included all 7 chromosomes).
-chrc.sizes=$scriptdir/TAIR10_gen_chrc.genome.sizes
-#
+#reference sequence directory variable - user should create a link called TAIR10_gen_chrc.chrom.sizes in script dir that points to the TAIR10_gen_chrc.genome.sizes file, or put a copy in there (chrc means we included all 7 chromosomes).
+
+chrc_sizes=${scriptdir}/TAIR10_gen_chrc.chrom.sizes
+
 
 sample=$1
 sample_dir=align/$sample
@@ -26,7 +27,7 @@ outdir="tdf_for_igv/${sample}"
 mkdir ${outdir}
 
 echo "bam to bedgraph"
-genomeCoverageBed -bg -strand -ibam $sample_dir/*.bam -g $chrc.sizes > $oudir/$sample.bedgraph
+bedtools genomecov -bg -ibam $sample_dir/*.bam -g $chrc_sizes > $outdir/${sample}.bedgraph
 
 echo "bedgraph to binary tiled data (.tdf) file"
-igvtools toTDF $sample_dir/${sample}.bedgraph $oudir/$sample.tdf chrc.sizes
+igvtools toTDF $outdir/*.bedgraph $outdir/$sample.tdf $chrc_sizes
